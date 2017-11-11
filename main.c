f3e3b5b174c759b04fbc1c3f3eac4dba9151f0ae
Araggar <lucas.mayr.athayde@gmail.com>
2017-11-11 15:59:13 -0200

    Add bucket sizes

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <float.h>
+#include <stdlib.h>
 
-void print_array(const unsigned int*, double*);
+void print_array(const unsigned int*, int*);
+int compare_double(const void *x, const void *y);
+int compare_int(const void *x, const void *y);
 
 int main(int argc, char** argv) {
 	if (argc < 5) {
@@ -11,17 +15,17 @@ int main(int argc, char** argv) {
 		exit(1);
 	}
 
+	// Parameters
+
 	const unsigned int ARRAY_SIZE = atoi(argv[1]);
 	const unsigned int NUMBER_OF_BUCKETS = atoi(argv[2]);
 	const unsigned int NUMBER_OF_PROCESSES = atoi(argv[3]);
 	const unsigned int PRINT_ORIGINAL = atoi(argv[4]);
 
-	double ORIGINAL[ARRAY_SIZE];
+	int ORIGINAL[ARRAY_SIZE];
 
-	printf("Array size : %u\n"
-		"Number of buckets: %u\n"
-		"Number of processes %u\n",
-		ARRAY_SIZE, NUMBER_OF_BUCKETS, NUMBER_OF_PROCESSES);
+	double max_n = ARRAY_SIZE;
+	double min_n = 0;
 
 	for (int i = 0; i < ARRAY_SIZE; i++) {
 		if (scanf("%d", &ORIGINAL[i]) != 1) {
@@ -30,10 +34,48 @@ int main(int argc, char** argv) {
 		}
 	}
 
+	// Bucket Bounds
+
+	int bucket_sizes[NUMBER_OF_BUCKETS];
+	int bucket_std_size = ARRAY_SIZE / NUMBER_OF_BUCKETS;
+	int extra = ARRAY_SIZE % NUMBER_OF_BUCKETS;
+
+	for (int i = 0 ; i < NUMBER_OF_BUCKETS; i++) {
+		if (extra) {
+			bucket_sizes[i] = bucket_std_size + 1;
+			--extra;
+		} else {
+			bucket_sizes[i] = bucket_std_size;
+		}
+	}
+
+	// Print parameters
+
+	printf("Array size : %u\n"
+		"Number of buckets: %u\n"
+		"Number of processes %u\n"
+		"Min: %f || Max: %f\n",
+		ARRAY_SIZE, NUMBER_OF_BUCKETS, NUMBER_OF_PROCESSES,
+		min_n, max_n);
+
+
+	// Bucket 
+
+
+
+
 
 	if (PRINT_ORIGINAL) {
 		print_array(&ARRAY_SIZE, ORIGINAL);
 	}
+
+	qsort(ORIGINAL, ARRAY_SIZE, sizeof(int), compare_int);
+
+	print_array(&ARRAY_SIZE, ORIGINAL);
+
+	// Find bucket bounds
+
+
 
 
 
@@ -48,11 +90,26 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-void print_array(const unsigned int* size, double* array) {
+void print_array(const unsigned int* size, int* array) {
 	printf("(");
 	for (int i = 0; i < *size; i++) {
 		printf("%d, ", array[i]);
 	}
 	printf("\b\b)\n");
 	return;
+}
+
+// Not working, don't know why yet
+int compare_double(const void *x, const void *y) {
+	double* _x = (double*) x;
+	double* _y = (double*) y;
+	if (*_x < *_y) return -1;
+	else if (*_x > *_y) return 1;
+	return 0;
+}
+
+int compare_int(const void *x, const void *y) {
+	int* _x = (int*) x;
+	int* _y = (int*) y;
+	return *_x - *_y;
 }
