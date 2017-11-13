6b87b94bf2ad1b61dced909645324d897f65ce47
Luiz João Motta <luizjoaomotta@gmail.com>
2017-11-13 21:03:08 -0200

    programa finalizado, sem bugs

--- main.c
+++ main.c
@@ -22,22 +22,6 @@ int compare_double(const void *x, const void *y);
 
 int compare_int(const void *x, const void *y);
 
-// Bucket Bounds (NON-INCLUSIVE)
-int *set_bounds(const unsigned int ARRAY_SIZE, 
-				const unsigned int NUMBER_OF_BUCKETS);
-
-// Count bucket sizes and store the Original array
-int *set_bucket_sizes(int *bucket_bounds,
-				      const unsigned int ARRAY_SIZE,
-				      const unsigned int NUMBER_OF_BUCKETS,
-				      int *ORIGINAL);
-
-// Build & Fill the buckets
-List *build_buckets(int *bucket_bounds, int *bucket_array_sizes,
-				   const unsigned int ARRAY_SIZE, 
-				   const unsigned int NUMBER_OF_BUCKETS, 
-				   int *ORIGINAL);
-
 int main(int argc, char** argv) {
 
 	if (argc < 4) {
@@ -61,6 +45,9 @@ int main(int argc, char** argv) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+	// processo 0 salva o array original e faz um broadcast para os outros processos
+	// ------------------------------------------------------------------------
+
 	int ORIGINAL[ARRAY_SIZE];
 	if (rank == 0) {
 		for (int i = 0; i < ARRAY_SIZE; i++) {
@@ -73,7 +60,8 @@ int main(int argc, char** argv) {
 
 	MPI_Bcast(&ORIGINAL, ARRAY_SIZE, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // -------------------------------------------------------------------------------------
+	// Bucket Bounds (NON-INCLUSIVE)
+	// ------------------------------------------------------------------------
 
     int bucket_bounds[NUMBER_OF_BUCKETS];
 
@@ -96,9 +84,8 @@ int main(int argc, char** argv) {
 		}
 	}
 
-	//print_array(&NUMBER_OF_BUCKETS, bucket_bounds); //  DEBUG
-
-	// -------------------------------------------------------------------------------------
+    // Count bucket sizes and store the Original array
+	// ------------------------------------------------------------------------
 
 	int bucket_array_sizes[NUMBER_OF_BUCKETS];
 
@@ -114,9 +101,8 @@ int main(int argc, char** argv) {
 		}
 	}
 
-	//print_array(&NUMBER_OF_BUCKETS, bucket_array_sizes); //  DEBUG
-
-	// -------------------------------------------------------------------------------------
+	// Build & Fill the buckets
+	// ------------------------------------------------------------------------
 
 	List buckets[NUMBER_OF_BUCKETS];
 
@@ -133,85 +119,73 @@ int main(int argc, char** argv) {
 		}
 	}
 
-	//for (int i = 0; i < NUMBER_OF_BUCKETS; i++)
-	//		print_buckets(bucket_array_sizes[i], buckets[i].int_list);
-
-	/*for (int i = 0; i < NUMBER_OF_BUCKETS; i++) {
-		print_array(&bucket_array_sizes[i], (buckets[i].int_list));  // DEBUG
-	}*/
-
-	// -------------------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
 		
 	int* bucket_tag_buffer;
 	int bucket_id;
 	unsigned int current_bucket = 0;
 	unsigned int remaining_buckets = NUMBER_OF_BUCKETS;
 
-	// ------------------------------------------------------------------------------------
+	// ------------------------------------------------------------------------
 
+	// processo mestre
 	if (rank == 0) {
 
-		/*print_parameters(ARRAY_SIZE, NUMBER_OF_BUCKETS, min_n, max_n);
-		if (PRINT_ORIGINAL) {
-			print_array(&ARRAY_SIZE, ORIGINAL);
-		}*/
-
-		// se tiver só um processo
+		// se tiver só um processo ou apenas um bucket
 		if (size == 1 || NUMBER_OF_BUCKETS == 1) {
 			for (int i = 0; i < NUMBER_OF_BUCKETS; i++) {
-				if (bucket_array_sizes[i] != 0) {
+				if (bucket_array_sizes[i] > 1) {
 					qsort(buckets[i].int_list, bucket_array_sizes[i], sizeof(int), compare_int);
 				}
 			}
 			current_bucket = NUMBER_OF_BUCKETS;
 			remaining_buckets = 0;
 
+		// caso tenha mais processos e/ou mais buckets
 		} else {
-			// send message
 
-			// distribuindo um bucket para todos os processos
+			// caso o numero de processo for maior que o de buckets
 			if (size > NUMBER_OF_BUCKETS) {
+
+				// envia os buckets iniciais
 				for (int i = 1; i < NUMBER_OF_BUCKETS + 1; i++) {
-					if (bucket_array_sizes[current_bucket]) {
+					if (bucket_array_sizes[current_bucket] > 1) {
 						MPI_Send(&current_bucket, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
 					}
 					current_bucket++;
 				}
 
+				// espera o retorno dos buckets ordenados
 				while (remaining_buckets != 0) {
 					MPI_Recv(&bucket_id, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
 					MPI_Recv(buckets[bucket_id].int_list, bucket_array_sizes[bucket_id], MPI_INT, status.MPI_SOURCE, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 					remaining_buckets--;
 				}
 
+			// caso tenha mais ou o mesmo numero de buckets igual ao de processos
 			} else {
 
+				// envia os buckets iniciais
 				for (int i = 1; i < size ; i++) {
-					if (bucket_array_sizes[current_bucket]) {
+					if (bucket_array_sizes[current_bucket] > 1) {
 						MPI_Send(&current_bucket, 1, MPI_INT, i, 0, MPI_COMM_WORLD); // envia id do bucket
-						//printf("Rank 0 sent a bucket to Rank %i\n", i);
 					}
 					current_bucket++;
 				}
 
-				while (remaining_buckets != 0 && current_bucket <= NUMBER_OF_BUCKETS) { //remaining_buckets != 0
-					// recebe de um escravo			
+				// enquanto tiver buckets ele irá continuar recebendo eles ordenado, e enviando um novo
+				while (remaining_buckets != 0 && current_bucket <= NUMBER_OF_BUCKETS) {
+
+					// recebe o bucket do escravo
 					MPI_Recv(&bucket_id, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
-					//printf("Received bucket : %i\n", bucket_id);
 					MPI_Recv(buckets[bucket_id].int_list, bucket_array_sizes[bucket_id], MPI_INT, status.MPI_SOURCE, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-					/*for (int i = 0; i < NUMBER_OF_BUCKETS; i++)
-						print_buckets(bucket_array_sizes[i], buckets[i].int_list);
-					printf("\n");*/
-					//print_array(&bucket_array_sizes[bucket_id], buckets[bucket_id].int_list);
-					// fflush(stdout);
 					remaining_buckets--;
 
-					//envia para esse mesmo escravo um novo bucket
-
+					//envia para esse mesmo escravo um novo bucket valido
 					while (remaining_buckets != 0 && current_bucket <= NUMBER_OF_BUCKETS) {
-						if (bucket_array_sizes[current_bucket]) {
-							// printf("sending %i\n", current_bucket);
+						if (bucket_array_sizes[current_bucket] > 1) {
 							MPI_Send(&current_bucket, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
+							remaining_buckets--;
 							current_bucket++;
 							break;
 						}
@@ -222,39 +196,43 @@ int main(int argc, char** argv) {
 			}
 		}
 
+		// envia ID = -1, sinalizando pra os outros processo que não tem mais buckets.
 		bucket_id = -1;
 		for (int i = 1; i < size; i++) {
-			MPI_Send(&bucket_id, 1, MPI_INT, i, 0, MPI_COMM_WORLD); // sinaliza os processos que nao existem mais buckets
+			MPI_Send(&bucket_id, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
 		}
 
+		// imprime o array original
 		if (PRINT_ORIGINAL) {
 			print_array(&ARRAY_SIZE, ORIGINAL);
 		}
 
+		// imprime o array ordenado
 		for (int i = 0; i < NUMBER_OF_BUCKETS; i++)
 			if (bucket_array_sizes[i] != 0)
 				print_buckets(bucket_array_sizes[i], buckets[i].int_list);
 
 		printf("\n");
-
-		//print_array(&ARRAY_SIZE, ORIGINAL);
 	}
 
-	// recebe os buckets e ordena ate receber bcast do 
+	// processos escravos
 	else {
+
 		while(1) {
-			MPI_Recv(&current_bucket, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // guarda o tamanho do array
-			//print_array(&bucket_array_sizes[current_bucket], buckets[current_bucket].int_list);
-			// break if tag = -1
+
+			// recebe qual o ID do bucket a ser ordenado
+			MPI_Recv(&current_bucket, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+			// caso o ID seja -1, o processo escravo sai do loop e finaliza
 			if (current_bucket == -1) {
 				break;
 			}
-			qsort(buckets[current_bucket].int_list, bucket_array_sizes[current_bucket], sizeof(int), compare_int); // sort o array
-			//print_array(&bucket_array_sizes[current_bucket], buckets[current_bucket].int_list);
-			//printf("Rank %i recv a bucket from Rank 0\n", rank);
-			MPI_Send(&current_bucket, 1, MPI_INT, 0, 1, MPI_COMM_WORLD); // devolve o array sorted com a tag = bucket
-			//printf("CURRENT BUCKET %i\n", bucket_array_sizes[current_bucket]);
 
+			// ordena o bucket
+			qsort(buckets[current_bucket].int_list, bucket_array_sizes[current_bucket], sizeof(int), compare_int); // sort o array
+
+			// envia o ID do bucket ordenado e o próprio bucket.
+			MPI_Send(&current_bucket, 1, MPI_INT, 0, 1, MPI_COMM_WORLD); // devolve o array sorted com a tag = bucket
 			MPI_Send(buckets[current_bucket].int_list, bucket_array_sizes[current_bucket], MPI_INT, 0, 2, MPI_COMM_WORLD); // devolve o array sorted com a tag = bucket
 		}
 	}
@@ -302,73 +280,4 @@ int compare_int(const void *x, const void *y) {
 	int* _x = (int*) x;
 	int* _y = (int*) y;
 	return *_x - *_y;
-}
-
-int *set_bounds(const unsigned int ARRAY_SIZE, 
-				const unsigned int NUMBER_OF_BUCKETS) {
-
-	int *bucket_bounds = malloc(sizeof(int) * NUMBER_OF_BUCKETS);
-
-	int bucket_std_size = ARRAY_SIZE / NUMBER_OF_BUCKETS;
-	int extra = ARRAY_SIZE % NUMBER_OF_BUCKETS;
-
-	if (extra) {
-			bucket_bounds[0] = bucket_std_size + 1;
-			--extra;
-		} else {
-			bucket_bounds[0] = bucket_std_size;
-		}
-
-	for (int i = 1 ; i < NUMBER_OF_BUCKETS; i++) {
-		if (extra) {
-			bucket_bounds[i] = bucket_bounds[i-1] + bucket_std_size + 1;
-			--extra;
-		} else {
-			bucket_bounds[i] = bucket_bounds[i-1] + bucket_std_size;
-		}
-	}
-	return bucket_bounds;
-}
-
-int *set_bucket_sizes(int *bucket_bounds,
-				      const unsigned int ARRAY_SIZE,
-				      const unsigned int NUMBER_OF_BUCKETS,
-				      int *ORIGINAL) {
-
-	int *bucket_array_sizes = malloc(sizeof(int) * NUMBER_OF_BUCKETS);
-
-	for (int i = 0; i < NUMBER_OF_BUCKETS; i++) {
-		bucket_array_sizes[i] = 0;
-	}
-	for (int i = 0; i < ARRAY_SIZE; i++) {
-		for (int b = 0; b < NUMBER_OF_BUCKETS; b++) {
-			if (ORIGINAL[i] < bucket_bounds[b]) {
-				bucket_array_sizes[b]++;
-				break;
-			}
-		}
-	}
-	return bucket_array_sizes;
-}
-
-List *build_buckets(int *bucket_bounds, int *bucket_array_sizes,
-				   const unsigned int ARRAY_SIZE, 
-				   const unsigned int NUMBER_OF_BUCKETS, 
-				   int *ORIGINAL) {
-
-	List *buckets = malloc(sizeof(List) * NUMBER_OF_BUCKETS);
-
-	for (int i = 0; i < NUMBER_OF_BUCKETS; i++) {
-		intlist_init(&buckets[i], bucket_array_sizes[i]);
-	}
-
-	for (int i = 0; i < ARRAY_SIZE; i++) {
-		for (int b = 0; b < NUMBER_OF_BUCKETS; b++) {
-			if (ORIGINAL[i] < bucket_bounds[b]) {
-				intlist_append(&buckets[b], &ORIGINAL[i]);
-				break;
-			}
-		}
-	}
-	return buckets;
 }
