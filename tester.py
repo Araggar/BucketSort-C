82c82772130ce7a3bdc52f2309804f42c23041c5
Araggar <lucas.mayr@hotmail.com>
2017-11-14 17:45:28 -0200

    Adjust to tester.py, better layout for results

--- tester.py
+++ tester.py
@@ -1,9 +1,11 @@
 from timeit import timeit
 from sys import argv, maxsize
 from argparse import ArgumentParser
+
 import time
 import os.path
 import random_array
+import os
 
 def main():
 
@@ -22,8 +24,8 @@ def main():
 
 	args = parser.parse_args()
 
-	folder = str(int(time.time()))
-	os.mkdir(folder)
+	folder = os.path.join("tests",str(int(time.time())))
+	os.makedirs(folder)
 	path = os.path.join(folder, 'results.txt')
 	array_path = os.path.join(folder, '{}.in'.format(args.size))
 	random_array.main(args.size, array_path)
@@ -37,16 +39,17 @@ def main():
 	with open(path, 'w') as results:
 		for p in range (1, args.processes + 1):
 			for b in range(1, args.buckets + 1):
-				exec_str = "run('mpirun {} -n {} {} \
-					{} 0 < {}', stdout=trash)".format(
-					args.name, p, args.size, b, array_path)
-				_time = timeit(exec_str, number=1, setup='import os; from subprocess import run; trash=open(os.devnull, "w")')
+				exec_str = "call('mpirun -n {} {} {} {} 0 < {}', stdout=trash, shell=True)".format(
+					p, args.name, args.size, b, array_path)
+				_time = timeit(exec_str, number=1, setup='import os; from subprocess import call; trash=open(os.devnull, "w")')
 				if _time < best_time:
 					best_time = _time
 					best_pb = p, b
-				results.write("N_Pro.:{} | N_Buck.:{} | time:{:0.8f} ms\n".format(p, b, _time*1000))
+				results.write("N_Pro.:{} | N_Buck.:{} | time:{:0.15f} s\n".format(p, b, _time))
 				contador += 100
 				print("{:1.2f}%".format(contador/total), end="\r")
+		results.write("Best Processes/Buckets combo: P:{} B:{} with : {:0.15f} seconds".format(best_pb[0], best_pb[1], best_time))
+		print("\n\nBest Processes/Buckets combo: P:{} B:{}, with {:0.15f} seconds".format(best_pb[0], best_pb[1], best_time))
 
 if __name__ == "__main__":
 	main()
